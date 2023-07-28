3f4789ce101636e3f0254a1a3a93696567591acf
Studawatta <st.udawatte@gmail.com>
2023-07-28 23:52:06 +0530

    booking form added

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -4,7 +4,7 @@ import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 const Carousel = ({
   children: slides,
   autoSlide = false,
-  autoSlideInterval = 3000,
+  autoSlideInterval = 5000,
 }) => {
   const [curr, setCurr] = useState(0);
 
@@ -21,27 +21,14 @@ const Carousel = ({
   return (
     <div className=" relative w-full overflow-hidden">
       <div
-        className=" flex transition-transform duration-1000 ease-in-out"
+        className=" flex transition-transform duration-[2000ms] ease-in-out"
         style={{ transform: `translateX(-${curr * 100}%)` }}
       >
         {slides}
       </div>
-      <div className=" absolute inset-0 flex items-center justify-between p-4">
-        <button
-          onClick={prev}
-          className="rounded-full border-none bg-white/80 p-1 text-gray-800 shadow hover:bg-white"
-        >
-          <AiOutlineLeft size={40} />
-        </button>
-        <button
-          onClick={next}
-          className="rounded-full border-none bg-white/80 p-1 text-gray-800 shadow hover:bg-white"
-        >
-          <AiOutlineRight size={40} />
-        </button>
-      </div>
+      <div className=" absolute inset-0 flex items-center justify-between p-4"></div>
       <div className="absolute bottom-4 left-0 right-0">
-        <div className="flex items-center justify-center gap-2">
+        <div className="mb-32 flex items-center justify-center gap-2 ">
           {slides.map((_, i) => (
             <div
               className={`h-3 w-3 rounded-full bg-white transition-all ${
--- src/modules/Landing/index.jsx
+++ src/modules/Landing/index.jsx
@@ -6,20 +6,56 @@ const Landing = () => {
   const slides = [r1, r2, r3, r4];
   return (
     <div>
-      <div className="h-[600px] w-full bg-red-200">
-        {/* <div>
-          <h1>Welcome to Sri Lanka Railways</h1>
-          <p>Online Advance Train Seats Reservation</p>
-        </div> */}
-        {/* <img src={r4} className=" h-[600px] w-full object-cover" /> */}
-        <div className="w-full">
-          <Carousel autoSlide={false}>
+      <div className="relative h-[780px] w-full ">
+        <div className=" w-full">
+          <Carousel autoSlide={true}>
             {slides.map((r) => (
-              <img src={r} className=" h-[600px] min-w-full  object-cover" />
+              <img src={r} className=" h-[780px] min-w-full  object-cover" />
             ))}
           </Carousel>
         </div>
+        <div className="absolute top-[300px] ml-20 flex h-[180px] flex-col justify-center ">
+          <h1 className="text-[40px] font-semibold text-white drop-shadow-[0_8px_2px_black]">
+            Welcome to Sri Lanka Railways
+          </h1>
+          <p className="text-[24px] text-white drop-shadow-[0_8px_2px_black]">
+            Online Advance Train Seats Reservation
+          </p>
+          <a
+            href="#booking-form"
+            className="mt-4 w-fit cursor-pointer rounded-[30px] border-2 px-4 py-3 text-[18px] font-semibold text-white drop-shadow-[0_8px_2px_black] hover:bg-white hover:text-black hover:drop-shadow-none"
+          >
+            Book Your Seat
+          </a>
+        </div>
+        <div
+          id="booking-form"
+          className=" absolute left-0 right-0 top-[90%] mx-auto flex h-[200px] w-[70%] shadow-xl"
+        >
+          <div className="flex-[1] bg-blue-400">
+            <p className="ml-8 text-[32px] font-thin text-white">
+              Book Your Seat{" "}
+            </p>
+            <span className="ml-8 text-[16px] text-white">
+              You can book both ways
+            </span>
+          </div>
+          <div className="flex flex-[3] items-center justify-center bg-white">
+            <div className="h-[90%] w-[95%] bg-slate-400">
+              <div className="grid h-[70%] w-full grid-cols-3 gap-4 bg-black p-4">
+                <input className="h-8 w-[230px]" />
+                <input className="h-8 w-[230px]" />
+                <input className="h-8 w-[230px]" />
+                <input className="h-8 w-[230px]" />
+                <input className="h-8 w-[230px]" />
+                <input className="h-8 w-[230px]" />
+              </div>
+              <div className="ml-auto h-[30%] w-32 bg-green-300"></div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="h-40 w-full"></div>
     </div>
   );
 };
